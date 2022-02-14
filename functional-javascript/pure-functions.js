5271a3b104c4f7fd3d7fbcab075fa5c157e01f4d
Mikołaj Borowski <36244604+mikolajborowski@users.noreply.github.com>
2022-02-14 13:24:04 +0100

    funtionaljavascript - pure functions

---
+++ functional-javascript/pure-functions.js
@@ -0,0 +1,20 @@
+//Type of functions in JavaScript 
+
+//Mapping
+const sum = (a, b) => a + b
+
+//Procedure
+const update = (data) => {some code that updates db}
+
+// Input / Output
+const showMessage = (message) {some code that shows some message on users screen}
+
+//Pure function - function that is pure, that isnt update some variables, it only returns needed vaules
+let sum = 100;
+const sumFunct = (a, b) => a += b;
+sumFunct(sum, 100) //It will not update sum, it will only return 200
+// now if i need to update sum i can use it like this:
+sum = sumFunct(sum, 100)
+//Idempotent function - function that returns the same result for the same arguments
+//Referential transparency - whole body of function can be changed for som value, for example in case of testing
+
