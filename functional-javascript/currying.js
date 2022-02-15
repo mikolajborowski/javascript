d9233b8cfdd970a03bf1210aeaa86e50eb5f3e6f
Mikołaj Borowski <36244604+mikolajborowski@users.noreply.github.com>
2022-02-15 13:05:59 +0100

    functionaljavascript - currying

--- functional-javascript/currying.js
+++ functional-javascript/currying.js
@@ -18,3 +18,7 @@ curriedMultiply(2)(5);
 
 // Currying is a transformation of functions that translates a function from callable as f(a, b, c) into callable as f(a)(b)(c).
 // Currying doesn’t call a function. It just transforms it.
+//https://javascript.info/currying-partials
+//For example I have a function that takes 3 arguments. And my situation is that the first argument is always the same, so I can 
+//curry the function, then create a new function by setting first function with first argument with a fixed value, and then
+//i have completely new function that needs only 2 arguments, because first one is fixed and always be the same
